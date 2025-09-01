83b409deb83ca74e3f1536a70faadd720ae70d47
nexocite <93871603+nexocite@users.noreply.github.com>
2025-09-01 18:31:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT: If your repo name changes, update `base` to match the repo slug.
-// For this repo, it's '/Pepper-Mills-Studios/'.
 export default defineConfig({
   plugins: [react()],
-  base: "/Pepper-Mills-Studios/",
+  base: "/Pepper-Mills-Studios/", // must match repo name exactly
 });
